f7415de7231508a52a1568c8b0e121b2917aabda
Swapnil Mahajan <swapnilmahajan510@gmail.com>
2021-08-29 12:09:10 -0700

    Working Version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
   end
   def about
+  	@name = "Swapnil Mahajan"
+  	@message = "My name is #{@name}"
+  	@welcome = "Welcome to my App!!!!!!"
   end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  #get 'home/index'
+  devise_for :users
+  resources :friends
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  #root 'home#index'
+  root 'friends#index'
   get  'home/about'
 end
